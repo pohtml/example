4b82a54efe2f9cb119e61fc1c0febc2c36160e81
hiebr <hiebr@DESKTOP-HKVV1E5.home>
2023-06-13 10:51:35 +0200

    Error fix

--- servlet-container/src/main/java/servlets/Form.java
+++ servlet-container/src/main/java/servlets/Form.java
@@ -1,15 +1,13 @@
 package servlets;
 
-import com.github.pohtml.Get2;
+import com.github.pohtml.Get;
 import com.github.pohtml.Json;
-import com.github.pohtml.annotations.Resource;
+import com.github.pohtml.annotations.DynamicHtml;
 
-@Resource(uri = "/dbac/ui", context = "/intranet/dep/explotacion/arqudesa")
-public class Form extends Get2 {
-	
+@DynamicHtml(model = "/dbac/ui", view = "/common/intranet/dep/explotacion/arqudesa")
+public class Form extends Get {
 	@Override
 	public void run(Json response) {
-		response.value("greeting", "Hello, World!");
+		response.value("Hello, ", "World!");
 	}
-	
 }
--- servlet-container/src/main/java/servlets/GetServletForm.java
+++ servlet-container/src/main/java/servlets/GetServletForm.java
@@ -5,11 +5,11 @@ import javax.servlet.annotation.WebServlet;
 import com.github.pohtml.GetServlet;
 
 @WebServlet({"/dbac/ui", "/common/intranet/dep/explotacion/arqudesa/dbac/ui.html"})
-public class FormServlet extends GetServlet<Form> {
+public class GetServletForm extends GetServlet<Form> {
 
 	private static final long serialVersionUID = 1L;
 
-	public FormServlet() {
+	public GetServletForm() {
 		super(serialVersionUID);
 	}
 	
